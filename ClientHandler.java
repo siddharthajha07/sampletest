7cb800de88b35b193fe344ec2dba06d616cfac6e
siddharthajha07 <siddharthajha07@gmail.com>
2021-12-03 14:42:37 -0500

    commit 6

--- ClientHandler.java
+++ ClientHandler.java
@@ -1,4 +1,3 @@
-package com.chatRoom;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
--- Server.java
+++ Server.java
@@ -1,5 +1,3 @@
-package com.chatRoom;
-
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
